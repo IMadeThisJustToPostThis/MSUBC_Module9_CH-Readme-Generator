925412d85956fbbc51af6f7442c076c54d1848aa
IMadeThisJustToPostThis <matzielbiz@gmail.com>
2022-11-29 18:19:03 -0500

    heavy bug-fixing, refactored code, and added comments

---
+++ Assets/js/generateMarkdown.js
@@ -0,0 +1,97 @@
+// imports
+const licenseData = require('./licenseDatabase.json'); // import our json file containing the license data
+
+// function that either returns the license link or an empty string
+function renderLicenseLink(licenseInput) {
+  if (!licenseInput) return ''; // if there is no license, return empty string
+  const licenseUrl = licenseData.licenseRoot + licenseInput.endpoint; // obtain license link from our database
+  return licenseUrl; // return license link
+}
+
+// function that either returns the appropriate license badge or an empty string
+function renderLicenseBadge(licenseInput) {
+  if (!licenseInput) return ''; // if there is no license, return empty string
+  const badgeURL = licenseData.badgeTemplate.replace( // obtain badge template link from our database and assign it to the badgeURL variable
+    '@name', licenseInput.endpoint).replace('@color', licenseInput.badgeColor); // replace the endpoint and color placeholders in the link with our data
+  const licenseLinks = `[![License: ${licenseInput.name}](${badgeURL})](${renderLicenseLink(licenseInput)})`; // create string literal with the license name, badge link, and license link
+  return licenseLinks; // return string literal with license link information
+}
+
+// function that either returns the license section of the README or an empty string
+function renderLicenseSection(licenseInput) {
+  if (!licenseInput) return ''; // if there is no license, return empty string
+  const licenseDescriptor = licenseInput.default ? `\n${licenseInput.info}` : // if licenseInput.default is true, set to licenseinput.info (because that means the 'no-license' license was inputted)
+    `\nThis application is distributed under the [${licenseInput.name}](${renderLicenseLink(licenseInput)}) license.` // if licenseInput.default doesn't exist, generate a new license info field
+
+  const finalLicenseInfo = renderLicenseBadge(licenseInput) + licenseDescriptor; // combine the license badge and license descriptor into one section of information
+  return finalLicenseInfo; // return combined section
+}
+
+// function that generates the markdown
+function generateMarkdown(readmeInput) {
+  const licenseInformation = licenseData.licenseInfo.find(
+    choiceData => choiceData.name === readmeInput.license); // locate the appropriate license type by comparing each license type in the json by name
+
+  //console.log(licenseInformation);
+
+  // return string literal with generated markdown
+  // when renderLicenseSection is called, it passes the appropriate licenses data to the chain of sub-functions
+  const generatedMarkdown = `
+  # ${readmeInput.title}
+  ## Description
+  ${readmeInput.description}
+  ## License
+  ${renderLicenseSection(licenseInformation)}
+  ## Table of Contents
+  - [License](#License)
+  - [Usage](#Usage)
+  - [Installation](#Installation)
+  - [Testing](#Testing)
+  - [Contributing](#Contributing)
+  ## Usage
+  ${readmeInput.usage}
+  ## Installation
+  ${readmeInput.installation}
+  ## Tests
+  ${readmeInput.tests}
+  ## Contributing
+  ${readmeInput.contributing}
+  ## Questions
+  Github:<https://github.com/${readmeInput.user}>
+  \nEmail: ${readmeInput.email}
+  `;
+
+  return generatedMarkdown;
+}
+
+// test data and function call for testing
+/*
+ testData = {
+  title: 'd',
+  description: 'd',
+  installation: 'd',
+  usage: 'd',
+  contributing: 'd',
+  tests: 'd',
+  user: 'd',
+  email: 'd',
+  license: 'MIT'
+};
+testData2 = {
+  title: 'd',
+  description: 'd',
+  installation: 'd',
+  usage: 'd',
+  contributing: 'd',
+  tests: 'd',
+  user: 'd',
+  email: 'd',
+  license: 'No License'
+};
+generateMarkdown(testData);
+console.log('==================================================================');
+generateMarkdown(testData2);
+*/
+
+// export generateMarkdown function
+module.exports = generateMarkdown;
--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,13 +1,74 @@
-// TODO: Include packages needed for this application
+// import packages
+const fs = require('fs'); // fs to read and write external files
+const inquirer = require('inquirer'); // inquirer to provide console-based prompts for the user
+const generateMarkdown = require('./generateMarkdown.js') // import our custom package that generates a markdown based on data fed to it
+const licenseData = require('./licenseDatabase.json').licenseInfo; // import our custom json that contains all of the license data required for the readme
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Please enter the title of the README file: '
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter the description of your project: '
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please enter the installation instructions for your software: '
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Plase enter the use-cases of your software: '
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Please enter the projects guidelines for contribution: '
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please enter any test results: '
+    },
+    {
+        type: 'input',
+        name: 'user',
+        message: 'Please enter your github username: '
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please Enter your email: '
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please select from one of the following licenses: ',
+        choices: licenseData,
+    },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function that writes our file after the data has been generated
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => { // write the file
+        if (err) throw err; // if error is detected display it on the console
+        console.log('README has been generated in \'README.md\' file'); // confirmation statement that shows up in the console if no error is detected
+    })
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// function that initlializes the app
+function init() {
+    inquirer
+        .prompt(questions) // prompt the user our array of questions
+        .then(readmeData => generateMarkdown(readmeData)/*console.log(readmeData)*/) // feed that data to our generateMarkdown function imported from our generatemarkdown package
+        .then(markdownData => writeToFile('README.md', markdownData)/*console.log(markdownData)*/) // write the returned markdown to a README.md file
+}
 
-// Function call to initialize app
+// initialize the app
 init();
